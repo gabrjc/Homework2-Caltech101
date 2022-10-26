96b902a549d50d80df2e61ac214ec23f16e4c79d
gabrjc <71277117+gabrjc@users.noreply.github.com>
2022-10-26 17:13:17 +0200

    Update caltech_dataset.py

--- caltech_dataset.py
+++ caltech_dataset.py
@@ -18,9 +18,27 @@ class Caltech(VisionDataset):
     def __init__(self, root, split='train', transform=None, target_transform=None):
         super(Caltech, self).__init__(root, transform=transform, target_transform=target_transform)
 
-        self.split = split # This defines the split you are going to use
-                           # (split files are called 'train.txt' and 'test.txt')
+        self.split = split 
+        self.FileInput = os.getcwd()+"/Caltech101/"+split+".txt"
+        self.Img_list=[]
+        self.Label_list=[]  
+        self.Lenght=0
 
+        file1 = open(self.FileInput, 'r')
+        Lines = file1.readlines()
+  
+        
+        for line in Lines:
+          label = line.split("/")[0]
+          if label=="BACKGROUND_Google":
+            continue
+          
+          self.Img_list.append(pil_loader(root+"/"+line.strip()))
+          self.Label_list.append(label)
+          self.Lenght += 1
+
+        print("Loaded {} Images".format(self.Lenght,split+".txt"))
+        
         '''
         - Here you should implement the logic for reading the splits files and accessing elements
         - If the RAM size allows it, it is faster to store all data in memory
@@ -40,20 +58,12 @@ class Caltech(VisionDataset):
             tuple: (sample, target) where target is class_index of the target class.
         '''
 
-        image, label = ... # Provide a way to access image and label via index
-                           # Image should be a PIL Image
-                           # label can be int
-
-        # Applies preprocessing when accessing the image
+        image = self.Img_list[index]
+        label = self.Label_list[index]
         if self.transform is not None:
             image = self.transform(image)
 
         return image, label
 
     def __len__(self):
-        '''
-        The __len__ method returns the length of the dataset
-        It is mandatory, as this is used by several other components
-        '''
-        length = ... # Provide a way to get the length (number of elements) of the dataset
-        return length
+      return self.Lenght
